016f1eabbbf5370f8198893375752adc5266ae61
c10coding <caleb.ja.owens@gmail.com>
2020-12-28 13:56:22 -0500

    Commit before I re-structure my format of potions

--- src/main/java/net/dohaw/ucitems/init/ModPotions.java
+++ src/main/java/net/dohaw/ucitems/init/ModPotions.java
@@ -15,42 +15,69 @@ public class ModPotions {
 
     public static Map<Potion, List<PotionType>> potions = new HashMap<>();
 
-    public static final Potion AGILITY_EFFECT = new PotionBase("agility_potion", false, 8257395, 0, 0);
-    public static final PotionType DEFAULT_AGILITY_TYPE = new PotionType("agility_potion", new PotionEffect[] {new PotionEffect(AGILITY_EFFECT, 2400)}).setRegistryName("agility_potion");
+    private static final String AGILITY_POTION_NAME = "agility_potion";
+    private static final String GREATER_AGILITY_POTION_NAME = "greater_agility_potion";
+    private static final String CONFLAGRATION_POTION_NAME = "conflagration_potion";
+    private static final String GREATER_CONFLAGRATION_POTION_NAME = "greater_conflagration_potion";
+    private static final String LESSER_CURE_POTION_NAME = "lesser_cure_potion";
+    private static final String CURE_POTION_NAME = "cure_potion";
+    private static final String GREATER_CURE_POTION_NAME = "greater_cure_potion";
+    private static final String LESSER_EXPLOSION_POTION_NAME = "lesser_explosion_potion";
+    private static final String EXPLOSION_POTION_NAME = "explosion_potion";
+    private static final String GREATER_EXPLOSION_POTION_NAME = "greater_explosion_potion";
 
-    public static final Potion GREATER_AGILITY_EFFECT = new PotionBase("greater_agility_potion", false, 8236625, 0, 0);
-    public static final PotionType DEFAULT_GREATER_AGILITY_TYPE = new PotionType("greater_agility_potion", new PotionEffect[]{new PotionEffect(GREATER_AGILITY_EFFECT, 2400)}).setRegistryName("greater_agility_potion");
 
-    public static final Potion CONFLAGRATION_EFFECT = new PotionBase("conflagration_potion", false, 16728640, 0, 0);
-    public static final PotionType DEFAULT_CONFLAGRATION_TYPE = new PotionType("conflagration_potion", new PotionEffect[] {new PotionEffect(CONFLAGRATION_EFFECT, 2400)}).setRegistryName("conflagration_potion");
+    public static final Potion AGILITY_EFFECT = new PotionBase(AGILITY_POTION_NAME, false, 8257395, 0, 0);
+    public static final PotionType AGILITY_TYPE = new PotionType(AGILITY_POTION_NAME, new PotionEffect[] {new PotionEffect(AGILITY_EFFECT, 2400)}).setRegistryName(AGILITY_POTION_NAME);
 
-    public static final Potion GREATER_CONFLAGRATION_EFFECT = new PotionBase("greater_conflagration_potion", false, 10240576, 0, 0);
-    public static final PotionType DEFAULT_GREATER_CONFLGRATION_TYPE = new PotionType("greater_conflagration_potion", new PotionEffect[] {new PotionEffect(GREATER_CONFLAGRATION_EFFECT, 2400)}).setRegistryName("greater_conflagration_potion");
+    public static final Potion GREATER_AGILITY_EFFECT = new PotionBase(GREATER_AGILITY_POTION_NAME, false, 8236625, 0, 0);
+    public static final PotionType GREATER_AGILITY_TYPE = new PotionType(GREATER_AGILITY_POTION_NAME, new PotionEffect[]{new PotionEffect(GREATER_AGILITY_EFFECT, 2400)}).setRegistryName(GREATER_AGILITY_POTION_NAME);
 
-    public static final Potion LESSER_CURES_EFFECT = new PotionBase("lesser_cures_potion", false, 10595327, 0, 0);
-    public static final PotionType DEFAULT_LESSER_CURES_TYPE = new PotionType("lesser_cures_potion", new PotionEffect[] {new PotionEffect(LESSER_CURES_EFFECT, 2400)}).setRegistryName("lesser_cures_potion");
+    public static final Potion CONFLAGRATION_EFFECT = new PotionBase(CONFLAGRATION_POTION_NAME, false, 16728640, 0, 0);
+    public static final PotionType CONFLAGRATION_TYPE = new PotionType(CONFLAGRATION_POTION_NAME, new PotionEffect[] {new PotionEffect(CONFLAGRATION_EFFECT, 2400)}).setRegistryName(CONFLAGRATION_POTION_NAME);
 
-    public static final Potion CURES_EFFECT = new PotionBase("cures_potion", false, 7449599, 0, 0);
-    public static final PotionType DEFAULT_CURES_TYPE = new PotionType("cures_potion", new PotionEffect[] {new PotionEffect(CURES_EFFECT, 2400)}).setRegistryName("cures_potion");
+    public static final Potion GREATER_CONFLAGRATION_EFFECT = new PotionBase(GREATER_CONFLAGRATION_POTION_NAME, false, 10240576, 0, 0);
+    public static final PotionType GREATER_CONFLGRATION_TYPE = new PotionType(GREATER_CONFLAGRATION_POTION_NAME, new PotionEffect[] {new PotionEffect(GREATER_CONFLAGRATION_EFFECT, 2400)}).setRegistryName(GREATER_CONFLAGRATION_POTION_NAME);
+
+    public static final Potion LESSER_CURE_EFFECT = new PotionBase(LESSER_CURE_POTION_NAME, false, 7457023, 0, 0);
+    public static final PotionType LESSER_CURE_TYPE = new PotionType(LESSER_CURE_POTION_NAME, new PotionEffect[] {new PotionEffect(LESSER_CURE_EFFECT, 2400)}).setRegistryName(LESSER_CURE_POTION_NAME);
+
+    public static final Potion CURE_EFFECT = new PotionBase(CURE_POTION_NAME, false, 7449599, 0, 0);
+    public static final PotionType CURE_TYPE = new PotionType(CURE_POTION_NAME, new PotionEffect[] {new PotionEffect(CURE_EFFECT, 2400)}).setRegistryName(CURE_POTION_NAME);
+
+    public static final Potion GREATER_CURE_EFFECT = new PotionBase(GREATER_CURE_POTION_NAME, false, 7439103, 0, 0);
+    public static final PotionType GREATER_CURE_TYPE = new PotionType(GREATER_CURE_POTION_NAME, new PotionEffect[] {new PotionEffect(GREATER_CURE_EFFECT, 2400)}).setRegistryName(GREATER_CURE_POTION_NAME);
+
+    public static final Potion LESSER_EXPLOSION_EFFECT = new PotionBase(LESSER_EXPLOSION_POTION_NAME, false, 16760902, 0, 0);
+    public static final PotionType LESSER_EXPLOSION_TYPE = new PotionType(LESSER_EXPLOSION_POTION_NAME, new PotionEffect[] {new PotionEffect(LESSER_EXPLOSION_EFFECT, 2400)}).setRegistryName(LESSER_EXPLOSION_POTION_NAME);
+
+    public static final Potion EXPLOSION_EFFECT = new PotionBase(EXPLOSION_POTION_NAME, false, 16754502, 0, 0);
+    public static final PotionType EXPLOSION_TYPE = new PotionType(EXPLOSION_POTION_NAME, new PotionEffect[] {new PotionEffect(EXPLOSION_EFFECT, 2400)}).setRegistryName(EXPLOSION_POTION_NAME);
+
+    public static final Potion GREATER_EXPLOSION_EFFECT = new PotionBase(GREATER_EXPLOSION_POTION_NAME, false, 16749126, 0, 0);
+    public static final PotionType GREATER_EXPLOSION_TYPE = new PotionType(GREATER_EXPLOSION_POTION_NAME, new PotionEffect[] {new PotionEffect(GREATER_EXPLOSION_EFFECT, 2400)}).setRegistryName(GREATER_EXPLOSION_POTION_NAME);
+
+    public static final Potion GREATER_EXPLOSION_EFFECT = new PotionBase(GREATER_EXPLOSION_POTION_NAME, false, 16749126, 0, 0);
+    public static final PotionType GREATER_EXPLOSION_TYPE = new PotionType(GREATER_EXPLOSION_POTION_NAME, new PotionEffect[] {new PotionEffect(GREATER_EXPLOSION_EFFECT, 2400)}).setRegistryName(GREATER_EXPLOSION_POTION_NAME);
 
     public static void registerPotions(){
-        registerPotion(AGILITY_EFFECT, DEFAULT_AGILITY_TYPE, null);
-        registerPotion(GREATER_AGILITY_EFFECT, DEFAULT_GREATER_AGILITY_TYPE, null);
-        registerPotion(CONFLAGRATION_EFFECT, DEFAULT_CONFLAGRATION_TYPE, null);
-        registerPotion(GREATER_CONFLAGRATION_EFFECT, DEFAULT_GREATER_CONFLGRATION_TYPE, null);
-        registerPotion(LESSER_CURES_EFFECT, DEFAULT_LESSER_CURES_TYPE, null);
-        registerPotion(CURES_EFFECT, DEFAULT_CURES_TYPE, null);
+        registerPotion(AGILITY_EFFECT, AGILITY_TYPE);
+        registerPotion(GREATER_AGILITY_EFFECT, GREATER_AGILITY_TYPE);
+        registerPotion(CONFLAGRATION_EFFECT, CONFLAGRATION_TYPE);
+        registerPotion(GREATER_CONFLAGRATION_EFFECT, GREATER_CONFLGRATION_TYPE);
+        registerPotion(LESSER_CURE_EFFECT, LESSER_CURE_TYPE);
+        registerPotion(CURE_EFFECT, CURE_TYPE);
+        registerPotion(GREATER_CURE_EFFECT, GREATER_CURE_TYPE);
+        registerPotion(LESSER_EXPLOSION_EFFECT, LESSER_EXPLOSION_TYPE);
+        registerPotion(EXPLOSION_EFFECT, EXPLOSION_TYPE);
+        registerPotion(GREATER_EXPLOSION_EFFECT, GREATER_EXPLOSION_TYPE);
     }
 
-    private static void registerPotion(Potion effect, PotionType ...types){
+    private static void registerPotion(Potion effect, PotionType type){
         List<PotionType> validTypes = new ArrayList<>();
         ForgeRegistries.POTIONS.register(effect);
-        for(PotionType type : types){
-            if(type != null){
-                validTypes.add(type);
-                ForgeRegistries.POTION_TYPES.register(type);
-            }
-        }
+        validTypes.add(type);
+        ForgeRegistries.POTION_TYPES.register(type);
         potions.put(effect, validTypes);
     }
 
